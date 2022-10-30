40d7f10b045ccaeedf2d237e3332c02840ca21c0
Jose marcos <72453138+SakakibaraKun@users.noreply.github.com>
2022-10-29 23:36:57 -0300

    API Rest Examples in Asp.Net using .Net 6

---
+++ Program.cs
@@ -0,0 +1,82 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+var configuration = app.Configuration;
+ProductRepository.Init(configuration);
+
+
+//PADRÃO REST
+//CREATE
+app.MapPost("/products", (Product product) =>
+{
+    ProductRepository.Add(product);
+    return Results.Created($"/products/{product.Code}", product.Code);
+});
+
+//READ
+
+app.MapGet("/products/{code}", ([FromRoute] string code) =>
+{
+    var product = ProductRepository.GetBy(code);
+    if (product != null)
+        return Results.Ok(product);
+    return Results.NotFound();
+});
+
+//UPDATE
+app.MapPut("/products", (Product product) =>
+{
+    var productSaved = ProductRepository.GetBy(product.Code);
+    productSaved.Name = product.Name;
+    return Results.Ok;
+
+});
+//DELETE
+app.MapDelete("/products/{code}", ([FromRoute] string code) =>
+{
+    var productSaved = ProductRepository.GetBy(code);
+    ProductRepository.Remove(productSaved);
+    return Results.Ok;
+});
+
+if (app.Environment.IsStaging())
+    app.MapGet("/configuration/database", (IConfiguration configuration) =>
+    {
+        return Results.Ok($"{configuration["database:connection"]}/{configuration["database:port"]}");
+    });
+
+app.Run();
+
+public static class ProductRepository
+{
+
+    public static void Init(IConfiguration configuration)
+    {
+        var products = configuration.GetSection("Products").Get<List<Product>>();
+        Products = products;
+    }
+
+    public static List<Product> Products { get; set; } = Products = new List<Product>();
+
+    public static void Add(Product product)
+    {
+        Products.Add(product);
+    }
+
+    public static Product GetBy(string code)
+    {
+        return Products.FirstOrDefault(p => p.Code == code);
+    }
+
+    public static void Remove(Product product)
+    {
+        Products.Remove(product);
+    }
+}
+
+public class Product
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+}
