42d708bcdce93969c3bbee4613b9975f253e0cbb
Jose marcos <72453138+SakakibaraKun@users.noreply.github.com>
2022-11-01 18:51:57 -0300

    Dbcontext and connection database

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -79,4 +80,13 @@ public class Product
 {
     public string Code { get; set; }
     public string Name { get; set; }
+}
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer(
+            "Server=localhost;Database=Products;User Id=sa;Password=<Josenaruto14#>;MultipleActiveResultSets=true;Encrypt=YES;TrustServerCertificate=YES");
 }
